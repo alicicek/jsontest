fc9a3a9102daf6c1fd0a9082675baae3a29bd5ec
Ali Cicek <mralicicek@outlook.com>
2021-09-29 12:21:44 +0100

    pushing existing code

---
+++ script.js
@@ -0,0 +1,45 @@
+fetch('data.json')
+  .then(response => {
+    return response.json();
+  })
+  .then(function (data){
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log('error: ' + err);
+  });
+
+
+function appendData(data)
+{
+  var container = document.getElementById("displayData")
+  // var userContainer = document.getElementById("user")
+
+  for (let i = 0; i < data.length; i++) 
+  {
+    var userDiv = document.createElement("user");
+    var nameDiv = document.createElement("name");
+    var ageDiv = document.createElement("age");
+    var colourDiv = document.createElement("colour");
+
+    userDiv.classList.add("userBox");
+    nameDiv.classList.add("userField");
+    ageDiv.classList.add("userField");
+    colourDiv.classList.add("colourField");
+
+
+    nameDiv.innerHTML = "Name: " + data[i].name;
+    ageDiv.innerHTML = "Age: " + data[i].age;
+    colourDiv.innerHTML = "Favourite colour";
+    colourDiv.style.backgroundColor = data[i].colour;
+    
+    userDiv.appendChild(nameDiv);
+    userDiv.appendChild(ageDiv);
+    userDiv.appendChild(colourDiv);
+
+    container.appendChild(userDiv);
+
+  }
+
+}
+
